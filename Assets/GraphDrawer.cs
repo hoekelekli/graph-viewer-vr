ec5e7df3d0dd78c16c08f21bc483fb830b44516d
Hökelekli <Oemer.Hoekelekli@p3-group.com>
2019-06-18 18:07:41 +0200

    cleanup

--- Assets/GraphDrawer.cs
+++ Assets/GraphDrawer.cs
@@ -14,8 +14,8 @@ public class GraphDrawer : MonoBehaviour
     public GameObject lineGenerator;
     public GameObject nodeText;
     public GameObject arrowHead;
-    public string inputFile;
-
+    
+    private string inputFile = "Assets/Resources/gephi_ex_02.graphml";
     private List<Edge> edges = new List<Edge>();
     private List<Node> nodes = new List<Node>();
 
@@ -44,9 +44,20 @@ public class GraphDrawer : MonoBehaviour
                     {
                         case "node":
                             string id = child.Attributes[0].Value;
+
                             float size = float.Parse(child.ChildNodes[1].InnerText, CultureInfo.InvariantCulture);
-                            float[] rgb = {float.Parse(child.ChildNodes[2].InnerText, CultureInfo.InvariantCulture), float.Parse(child.ChildNodes[3].InnerText, CultureInfo.InvariantCulture), float.Parse(child.ChildNodes[4].InnerText, CultureInfo.InvariantCulture)};
-                            float[] xyz = {float.Parse(child.ChildNodes[5].InnerText, CultureInfo.InvariantCulture), float.Parse(child.ChildNodes[6].InnerText, CultureInfo.InvariantCulture), float.Parse(child.ChildNodes[7].InnerText, CultureInfo.InvariantCulture)};
+                            size = string.Equals(size.ToString("R"), "NaN") ? 1.0f : size;
+
+                            float red = float.Parse(child.ChildNodes[2].InnerText, CultureInfo.InvariantCulture);
+                            float green = float.Parse(child.ChildNodes[3].InnerText, CultureInfo.InvariantCulture);
+                            float blue = float.Parse(child.ChildNodes[4].InnerText, CultureInfo.InvariantCulture);
+                            float[] rgb = {red, green, blue};
+
+                            float x = float.Parse(child.ChildNodes[5].InnerText, CultureInfo.InvariantCulture);
+                            float y = float.Parse(child.ChildNodes[6].InnerText, CultureInfo.InvariantCulture);
+                            float z = float.Parse(child.ChildNodes[7].InnerText, CultureInfo.InvariantCulture);
+                            float[] xyz = {x, y, z};
+                            
                             nodes.Add(new Node(id, size, rgb, xyz));
                             break;
                         case "edge":
@@ -66,9 +77,19 @@ public class GraphDrawer : MonoBehaviour
         // Iterate through all nodes and draw from prefab.
         foreach(Node node in nodes)
         {
-            var n = Instantiate(ball, new Vector3(node.getXyz()[0] * 10, node.getXyz()[1] * 10, node.getXyz()[2] * 10), Quaternion.identity);
-            n.GetComponent<Renderer>().material.color = new Color(node.getRgb()[0]/255, node.getRgb()[1]/255, node.getRgb()[2]/255, 1.0f);
-            // n.transform.localScale = new Vector3(node.getSize(), node.getSize(), node.getSize());
+            float x = node.getXyz()[0] * 10;
+            float y = node.getXyz()[1] * 10;
+            float z = node.getXyz()[2] * 10;
+
+            float red = node.getRgb()[0] / 255;
+            float green = node.getRgb()[1] / 255;
+            float blue = node.getRgb()[2] / 255;
+
+            float size = node.getSize();
+
+            var n = Instantiate(ball, new Vector3(x, y, z), Quaternion.identity);
+            n.GetComponent<Renderer>().material.color = new Color(red, green, blue, 1.0f);
+            n.transform.localScale = new Vector3(size, size, size);
 
             var m = Instantiate(nodeText, n.transform.position, Quaternion.identity);
             TextMesh mesh = m.GetComponent<TextMesh>();
@@ -98,15 +119,18 @@ public class GraphDrawer : MonoBehaviour
         {
             GameObject newLineGenerator = Instantiate(lineGenerator);
             LineRenderer lineRenderer = newLineGenerator.GetComponent<LineRenderer>();
-            lineRenderer.SetWidth(0.1f, 0.1f); // todo: multiply with edge.getWeight()
+            lineRenderer.startWidth = 0.001f;
+            lineRenderer.endWidth = 0.001f;
             lineRenderer.SetPosition(0, edge.sourceNode.transform.position);
             lineRenderer.SetPosition(1, edge.destinationNode.transform.position);
             lineRenderer.startColor = Color.white;
             lineRenderer.endColor = Color.white;
 
             // add arrowhead
-            var arrow = Instantiate(arrowHead, edge.destinationNode.GetComponent<SphereCollider>().ClosestPoint(edge.sourceNode.transform.position), 
-                             Quaternion.LookRotation(edge.destinationNode.transform.position - edge.sourceNode.transform.position));
+            // var arrow = Instantiate(arrowHead, 
+            //             edge.destinationNode.GetComponent<SphereCollider>().ClosestPoint(edge.sourceNode.transform.position), 
+            //             Quaternion.LookRotation(edge.destinationNode.transform.position - edge.sourceNode.transform.position));
+            // arrow.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # graph-viewer-vr
-This Unity App plots Graphml files into 3D space.
+This Unity App plots graphml files into 3D space to be displayed on Android devices 
+in virtual reality.
+
+# How to use the App
+Just import this project in Unity (2019.3.0 or higher) including Standard Assets. To 
+view your own files insert your graphml files into the Assets/Resources directory and
+change the public variable inputFile in GraphDrawer class manually. 
+
+# How to get graphml files
+Use gephi version 8.2 with the Atlas 3D plugin to generate the graphml files. We make 
+use of gephi because it makes the math for us. 
+
+# Contribute to this project
+All kinds of contributions are welcomed. See Contribute.md for more details.
+For debugging setup see https://bertt.wordpress.com/2018/06/12/how-to-debug-your-unity3d-android-application-in-visual-studio/
 
 # Screenshots
-![screenshot](/Images/screenshot_01.png?raw=true "screenshot")
+![screenshot](/Images/screenshot_01.png?raw=true "screenshot 1")
